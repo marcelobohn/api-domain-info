9b55fe18676566c88723ac7f961a94436eaf48ca
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-06-26 01:15:56 -0300

    revisão para melhor aproveitamento da requisição

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-const { registerA, namesServers, whoisParse } = require('lib-domain-info');
+const { registerA, whoisParse } = require('lib-domain-info');
 
-router.get('/:domain', async function(req, res, next) {
-  const { domain } = req.params;
+router.get('/:domain/:type?', async function(req, res, next) {
+  let { domain, type } = req.params;
+  if (type === undefined) { type = 'all' };
+
+  let rtn = {};
+
   a = await registerA(domain);
-  ns = await namesServers(domain);
   w = await whoisParse(domain);
-  hostName = await whoisParse(a.address);
-  res.json({ registerA: a, namesServers: ns, whois: w, hostName: hostName });
+
+  if (type === 'all' || type === 'a') {
+    Object.assign(rtn, { registerA: a });
+  }
+
+  if (type === 'all' || type === 'ns') {
+    Object.assign(rtn, { namesServers: w.nameServer.split(' ') } );
+  }
+
+  if (type === 'all' || type === 'w') {
+    Object.assign(rtn, { whois: w });
+  }
+
+  if (type === 'all' || type === 'h') {
+    h = await whoisParse(a.address);
+    Object.assign(rtn, { hostName: h.owner });
+  }
+
+  res.json(rtn);
 });
 
 module.exports = router;
--- test/test-api.js
+++ test/test-api.js
@@ -8,13 +8,61 @@ chai.use(chaiHttp);
 
 describe("route", function() {
   describe('/GET /api/umbler.com', () => {
-    it('all domain information', (done) => {
+    it.only('all domain information', (done) => {
       chai.request(server)
         .get('/api/umbler.com')
         .end((err, res) => {
           expect(res.status).to.equal(200);
           expect(res.body).to.be.an('object');
+          expect(res.body).to.include.keys(['registerA', 'namesServers', 'whois', 'hostName']);
+          done();
+        });
+    });
 
+    it('register A of domain', (done) => {
+      chai.request(server)
+        .get('/api/umbler.com/a')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.include.keys(['registerA']);
+          expect(res.body).to.not.include.keys(['namesServers', 'whois', 'hostName']);
+          done();
+        });
+    });
+
+    it('names servers of domain', (done) => {
+      chai.request(server)
+        .get('/api/umbler.com/ns')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.include.keys(['namesServers']);
+          expect(res.body).to.not.include.keys(['registerA', 'whois', 'hostName']);
+          done();
+        });
+    });
+
+    it('whois of domain', (done) => {
+      chai.request(server)
+        .get('/api/umbler.com/w')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.include.keys(['whois']);
+          expect(res.body).to.not.include.keys(['registerA', 'namesServers', 'hostName']);
+          done();
+        });
+    });
+
+    it('host name of domain', (done) => {
+      chai.request(server)
+        .get('/api/umbler.com/h')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.include.keys(['hostName']);
+          expect(res.body).to.not.include.keys(['registerA', 'namesServers', 'whois']);
           done();
         });
     });
